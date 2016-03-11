a2cc59581b267a8475cc947d69c1d1c44c488efb
Samrat Shaw <footyapps27@gmail.com>
2016-03-11 13:22:05 +0800

    Added Login delegate for registering to events.

---
+++ UniversitySouvenirStore/src/sg/edu/nus/iss/universitystore/view/intf/ILoginDelegate.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package sg.edu.nus.iss.universitystore.view.intf;
+
+/**
+ * @author Samrat
+ *
+ */
+public interface ILoginDelegate {
+
+	/*
+	 * Declare the methods & add the default implementation to each method.
+	 */
+}
