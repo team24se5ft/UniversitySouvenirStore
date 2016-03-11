427f7956db0b6ac20e0cd305c7eb87cfbde39ff2
Samrat Shaw <footyapps27@gmail.com>
2016-03-11 15:30:17 +0800

    Added login controller test class.

---
+++ UniversitySouvenirStore/srcTest/sg/edu/nus/iss/universitystore/controller/test/LoginControllerTest.java
@@ -0,0 +1,24 @@
+package sg.edu.nus.iss.universitystore.controller.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class LoginControllerTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
