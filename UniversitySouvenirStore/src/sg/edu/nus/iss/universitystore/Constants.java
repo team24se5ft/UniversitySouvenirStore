a91b2245d52abf68a1f19f8ef2cb5d95a6bc2547
LinBoYuan <boyuan_lin@yahoo.com>
2016-03-11 13:47:49 +0800

    Add Constants
    It`s Global Constants

---
+++ UniversitySouvenirStore/src/sg/edu/nus/iss/universitystore/Constants.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.universitystore;
+
+public class Constants {
+	
+	private Constants() {
+	}
+
+	public final static String APP_NAME = "NUS STORE MANAGER";
+	// Window width in pixels
+	public final static int WINDOW_WIDTH = 800;
+	// Window height in pixels
+	public final static int WINDOW_HEIGHT = 800;
+}
