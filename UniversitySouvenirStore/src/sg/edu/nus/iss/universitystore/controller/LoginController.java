a273e34e40ddfd888bca7141c0e206a6ce2858ba
sanjayc <sanjay.choondal@u.nus.edu>
2016-03-12 22:54:52 +0800

    Updated Login Controller and Storekeeper to work with Login Tested Login Screen
    Signed-off-by: sanjayc <sanjay.choondal@u.nus.edu>

--- UniversitySouvenirStore/src/sg/edu/nus/iss/universitystore/controller/LoginController.java
+++ UniversitySouvenirStore/src/sg/edu/nus/iss/universitystore/controller/LoginController.java
@@ -1,9 +1,12 @@
 package sg.edu.nus.iss.universitystore.controller;
 
+import java.io.IOException;
+
 import javax.swing.JPanel;
 
 import sg.edu.nus.iss.universitystore.Constants;
 import sg.edu.nus.iss.universitystore.model.StoreKeeper;
+import sg.edu.nus.iss.universitystore.model.dao.impl.DataFileAccess;
 import sg.edu.nus.iss.universitystore.utility.UIUtils;
 import sg.edu.nus.iss.universitystore.utility.UIUtils.DialogType;
 import sg.edu.nus.iss.universitystore.view.LoginPanel;
@@ -49,36 +52,41 @@ public class LoginController implements ILoginDelegate {
 		//TODO
 		//FIXME - Read from file
 		//SMRT - Hard Coding now for 
-		if(username.equals("admin") && password.equals("admin")){
+		/*if(username.equals("admin") && password.equals("admin")){
 			//Move to dashboard screen
 			UIUtils.navigateToDashboard(loginPanel);
 		}
 		else {
 			UIUtils.showMessageDialog(loginPanel, Constants.STR_WARNING, STR_INCORRECT_LOGIN_MESSAGE, DialogType.WARNING_MESSAGE);
-		}
+		}*/
 		
-		
+		boolean isValidUser = false;
 		StoreKeeper storeKeeper = new StoreKeeper(username, password);
 		
-		//SMRT - Commented the below lines, waiting for Sanjay to commit the files.
-		
 		// Accessing Date from StoreKeeper.dat
-//		try {
-//			// DataFileImpl<StoreKeeper> storeKeeperData = new DataFileImpl<>(StoreConstants.STORE_KEEPER_DAT);
-//			DataFileAccess dataFileAccess = new DataFileAccess();
-//			// String[] dataStr = storeKeeperData.getAll();
-//			String[] dataStr = dataFileAccess.storeKeeperData.getAll();
-//			for(String line : dataStr){
-//				StoreKeeper storeKeeperDat = new StoreKeeper(line.split(","));
-//				if(storeKeeper.equals(storeKeeperDat)){
-//					System.out.println("Password Match !!!");
-//					break;
-//				}
-//			}
-//		} catch (IOException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
+		try {
+			// DataFileImpl<StoreKeeper> storeKeeperData = new DataFileImpl<>(StoreConstants.STORE_KEEPER_DAT);
+			DataFileAccess dataFileAccess = new DataFileAccess();
+			// String[] dataStr = storeKeeperData.getAll();
+			String[] dataStr = dataFileAccess.storeKeeperData.getAll();
+			for(String line : dataStr){
+				StoreKeeper storeKeeperDat = new StoreKeeper(line.split(","));
+				if(storeKeeper.equals(storeKeeperDat)){
+					System.out.println("Password Match !!!");
+					isValidUser = true;
+					break;
+				}
+			}
+			if(isValidUser){
+				UIUtils.navigateToDashboard(loginPanel);
+			}
+			else {
+				UIUtils.showMessageDialog(loginPanel, Constants.STR_WARNING, STR_INCORRECT_LOGIN_MESSAGE, DialogType.WARNING_MESSAGE);
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 	/***********************************************************/
--- UniversitySouvenirStore/src/sg/edu/nus/iss/universitystore/model/StoreKeeper.java
+++ UniversitySouvenirStore/src/sg/edu/nus/iss/universitystore/model/StoreKeeper.java
@@ -1,6 +1,9 @@
 package sg.edu.nus.iss.universitystore.model;
 
 public class StoreKeeper {
+	
+	private final static int USERNAME = 0;
+	private final static int PASSWORD = 1;
 
 	/***********************************************************/
 	//Instance Variables
@@ -14,6 +17,10 @@ public class StoreKeeper {
 	public StoreKeeper(String userName,String password) {
 		this.userName = userName;
 		this.password = password;
+	}
+	
+	public StoreKeeper(String[] args){
+		this(args[USERNAME],args[PASSWORD]);
 	}
 	
 	/***********************************************************/
@@ -43,6 +50,39 @@ public class StoreKeeper {
 	 */
 	@Override
 	public String toString() {
-		return "Storekeeper [name=" + userName + ", password=" + password + "]";
+		return userName + "," + password;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((password == null) ? 0 : password.hashCode());
+		result = prime * result + ((userName == null) ? 0 : userName.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		StoreKeeper other = (StoreKeeper) obj;
+		if (password == null) {
+			if (other.password != null)
+				return false;
+		} else if (!password.equals(other.password))
+			return false;
+		if (userName == null) {
+			if (other.userName != null)
+				return false;
+		} else if (!userName.equals(other.userName))
+			return false;
+		return true;
+	}
+	
+	
 }
