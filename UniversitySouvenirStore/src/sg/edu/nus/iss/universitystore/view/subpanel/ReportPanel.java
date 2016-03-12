a591a9b660ff42dd4e975b7823b156439070d086
LinBoYuan <boyuan_lin@yahoo.com>
2016-03-12 15:05:16 +0800

    add basic constructor to each subpanel

--- UniversitySouvenirStore/src/sg/edu/nus/iss/universitystore/view/subpanel/ReportPanel.java
+++ UniversitySouvenirStore/src/sg/edu/nus/iss/universitystore/view/subpanel/ReportPanel.java
@@ -1,7 +1,13 @@
 package sg.edu.nus.iss.universitystore.view.subpanel;
 
+import java.awt.Color;
+
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class ReportPanel extends JPanel{
-
+	public ReportPanel(){
+		setBackground(Color.GREEN);
+		add(new JLabel("report"));
+	}
 }
