977a68cb97a505f0e525f65cb7c6a4ccc76a3207
LinBoYuan <boyuan_lin@yahoo.com>
2016-03-14 21:13:18 +0800

    update DiscountPanel

--- UniversitySouvenirStore/src/sg/edu/nus/iss/universitystore/view/subpanel/DiscountPanel.java
+++ UniversitySouvenirStore/src/sg/edu/nus/iss/universitystore/view/subpanel/DiscountPanel.java
@@ -4,9 +4,13 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
 
@@ -32,19 +36,21 @@ public class DiscountPanel extends JPanel {
 	}
 
 	private void initDiscountTable() {
-		String[] headers = { "title1", "title2", "title3" };
-		Object[][] cellData = { { "row1-col1", "row1-col2", "row1-col3" }, { "row2-col1", "row2-col2", "row2-col3" } };
-
-		DefaultTableModel model = new DefaultTableModel(cellData, headers) {
-
+		String[] headers = { "name", "percentage", "type", "description" };
+		String[] content = { "Holiday", "20%", "member", "holiday celebration" };
+		String data[][] = { content };
+		DefaultTableModel model = new DefaultTableModel(data, headers) {
 			public boolean isCellEditable(int row, int column) {
 				return false;
 			}
 		};
-
 		DiscountTable = new JTable(model);
-		DiscountTable.getTableHeader().setVisible(true);
-		add(DiscountTable, "Center");
+		DiscountTable.setRowHeight(40);
+		DiscountTable.setBorder(BorderFactory.createEtchedBorder());
+		DiscountTable.setGridColor(Color.BLACK);
+		DiscountTable.setIntercellSpacing(new Dimension(1, 1));
+		JScrollPane JSP = new JScrollPane(DiscountTable);
+		add(JSP, "Center");
 	}
 
 	private void initButtonPanel() {
@@ -63,6 +69,28 @@ public class DiscountPanel extends JPanel {
 		buttonPanel.add(btnAdd);
 		buttonPanel.add(btnDelete);
 		add(buttonPanel, "South");
+		initButtonEvent();
+	}
+
+	private void initButtonEvent() {
+		btnDelete.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				DefaultTableModel model = (DefaultTableModel) DiscountTable.getModel();
+				model.removeRow(DiscountTable.getSelectedRow());
+			}
+		});
+		btnAdd.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// after comfirm dialog
+				DefaultTableModel model = (DefaultTableModel) DiscountTable.getModel();
+				model.addRow(new Object[] { "HelloWorld", "100%", "public", "free whole day!!!" });
+			}
+		});
+
 	}
 
 }
