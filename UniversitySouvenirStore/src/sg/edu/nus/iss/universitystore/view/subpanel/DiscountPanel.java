f03ea79d10dcfe7409a9da9d93b16f62fed85bd4
LinBoYuan <boyuan_lin@yahoo.com>
2016-03-14 11:16:20 +0800

    Update DiscountPanel and SalesPanel

--- UniversitySouvenirStore/src/sg/edu/nus/iss/universitystore/view/subpanel/DiscountPanel.java
+++ UniversitySouvenirStore/src/sg/edu/nus/iss/universitystore/view/subpanel/DiscountPanel.java
@@ -1,13 +1,68 @@
 package sg.edu.nus.iss.universitystore.view.subpanel;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
 
-import javax.swing.JLabel;
+import javax.swing.JButton;
 import javax.swing.JPanel;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
 
-public class DiscountPanel extends JPanel{
-	public DiscountPanel(){
+public class DiscountPanel extends JPanel {
+	private JTable DiscountTable;
+
+	private JButton btnActive;
+	private JButton btnStop;
+	private JButton btnAdd;
+	private JButton btnDelete;
+
+	private JPanel buttonPanel;
+
+	public DiscountPanel() {
 		setBackground(Color.WHITE);
-		add(new JLabel("discount"));
+		BorderLayout borderLayout = new BorderLayout();
+		borderLayout.setHgap(20);
+		borderLayout.setVgap(20);
+		this.setLayout(borderLayout);
+		initDiscountTable();
+		initButtonPanel();
+		// add(new JLabel("discount"));
 	}
+
+	private void initDiscountTable() {
+		String[] headers = { "title1", "title2", "title3" };
+		Object[][] cellData = { { "row1-col1", "row1-col2", "row1-col3" }, { "row2-col1", "row2-col2", "row2-col3" } };
+
+		DefaultTableModel model = new DefaultTableModel(cellData, headers) {
+
+			public boolean isCellEditable(int row, int column) {
+				return false;
+			}
+		};
+
+		DiscountTable = new JTable(model);
+		DiscountTable.getTableHeader().setVisible(true);
+		add(DiscountTable, "Center");
+	}
+
+	private void initButtonPanel() {
+		buttonPanel = new JPanel();
+		buttonPanel.setLayout(new FlowLayout());
+		btnActive = new JButton("Active");
+		btnActive.setPreferredSize(new Dimension(120, 40));
+		btnStop = new JButton("Stop");
+		btnStop.setPreferredSize(new Dimension(120, 40));
+		btnAdd = new JButton("Add");
+		btnAdd.setPreferredSize(new Dimension(120, 40));
+		btnDelete = new JButton("Delete");
+		btnDelete.setPreferredSize(new Dimension(120, 40));
+		buttonPanel.add(btnActive);
+		buttonPanel.add(btnStop);
+		buttonPanel.add(btnAdd);
+		buttonPanel.add(btnDelete);
+		add(buttonPanel, "South");
+	}
+
 }
--- UniversitySouvenirStore/src/sg/edu/nus/iss/universitystore/view/subpanel/SalesPanel.java
+++ UniversitySouvenirStore/src/sg/edu/nus/iss/universitystore/view/subpanel/SalesPanel.java
@@ -1,13 +1,95 @@
 package sg.edu.nus.iss.universitystore.view.subpanel;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.List;
 
+import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
-public class SalesPanel extends JPanel{
-	public SalesPanel(){
-		setBackground(Color.RED);
-		add(new JLabel("sales"));
+public class SalesPanel extends JPanel {
+	private List productList;
+
+	private JLabel memberOption;// add memberDialog to check member
+	private JTextField LoyalPointOption; // key in loyalPoint
+	private JLabel avaiLableLoyalPoint; // show avaliable loyalPoint
+	private JLabel discountOption;
+
+	private JButton addProductButton;
+	private JButton cancelButton;
+	private JButton checkOutButton;
+
+	private JPanel buttonPanel;
+	private JPanel customerInfoPanel;
+	private JPanel saleContentPanel;
+
+	public SalesPanel() {
+		BorderLayout borderLayout = new BorderLayout();
+		borderLayout.setHgap(20);
+		borderLayout.setVgap(20);
+		this.setLayout(borderLayout);
+		initSalePanel();
+		initButtonPanel();
+
 	}
+
+	private void initSalePanel() {
+		saleContentPanel = new JPanel();
+		productList = new List();
+		saleContentPanel.setBackground(Color.WHITE);
+		saleContentPanel.setLayout(new BoxLayout(saleContentPanel, BoxLayout.X_AXIS));
+//		saleContentPanel.setLayout(new GridLayout(1, 2));
+		saleContentPanel.add(productList);
+		initCustomerInfoPabel();
+		add(saleContentPanel, "Center");
+	}
+
+	private void initCustomerInfoPabel() {
+		customerInfoPanel = new JPanel();
+		customerInfoPanel.setBackground(Color.WHITE);
+		customerInfoPanel.setLayout(new GridLayout(6, 6));
+		customerInfoPanel.setPreferredSize(new Dimension(400, 300));
+		customerInfoPanel.setMaximumSize(new Dimension(400, 300));
+		customerInfoPanel.setAlignmentX(Component.LEFT_ALIGNMENT);
+		customerInfoPanel.add(new JLabel("member:"));
+		memberOption = new JLabel("Public");
+		memberOption.setForeground(Color.BLUE);
+		customerInfoPanel.add(memberOption);
+		customerInfoPanel.add(new JLabel("discount:"));
+		discountOption = new JLabel("null");
+		discountOption.setForeground(Color.BLUE);
+		customerInfoPanel.add(discountOption);
+		customerInfoPanel.add(new JLabel("AvailableLoyalPoint:"));
+		avaiLableLoyalPoint = new JLabel("0");
+		customerInfoPanel.add(avaiLableLoyalPoint);
+		customerInfoPanel.add(new JLabel("loyalPoint:"));
+		LoyalPointOption = new JTextField(5);
+		customerInfoPanel.add(LoyalPointOption);
+		saleContentPanel.add(customerInfoPanel);
+	}
+
+	private void initButtonPanel() {
+		buttonPanel = new JPanel();
+		buttonPanel.setLayout(new FlowLayout());
+		addProductButton = new JButton("Add Product");
+		addProductButton.setPreferredSize(new Dimension(120, 40));
+		checkOutButton = new JButton("Check OUT");
+		checkOutButton.setPreferredSize(new Dimension(120, 40));
+		cancelButton = new JButton("Cancel");
+		cancelButton.setPreferredSize(new Dimension(120, 40));
+		buttonPanel.add(addProductButton);
+		buttonPanel.add(checkOutButton);
+		buttonPanel.add(cancelButton);
+		add(buttonPanel, "South");
+	}
+
 }
