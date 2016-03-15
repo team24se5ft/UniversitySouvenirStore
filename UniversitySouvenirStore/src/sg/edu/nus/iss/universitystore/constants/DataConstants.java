147a0d3ee7ca80b957448947a1fd1fcef5f72e38
Deeapk Raaj <e0013445@u.nus.edu>
2016-03-15 10:33:44 +0800

    Added Constants for Loyalty points for new member
    Changed code according to standard

--- UniversitySouvenirStore/src/sg/edu/nus/iss/universitystore/constants/DataConstants.java
+++ UniversitySouvenirStore/src/sg/edu/nus/iss/universitystore/constants/DataConstants.java
@@ -13,4 +13,6 @@ public final class DataConstants {
 	public static final String DAT_FILE_PATH = System.getProperty("user.dir") + "\\data\\";
 	public static final String DAT_FILE_SEPRTR = ",";
 	public static final String DAT_FILE_EMPTY = "";
+	public static final int LOYALTY_NEW_MEMBER = -1;
+	public static final String MEMBER_NOT_FOUND = "Member not found for the given member id";
 }
--- UniversitySouvenirStore/src/sg/edu/nus/iss/universitystore/data/MemberManager.java
+++ UniversitySouvenirStore/src/sg/edu/nus/iss/universitystore/data/MemberManager.java
@@ -58,13 +58,9 @@ public class MemberManager {
 			if (memberLine.isEmpty())
 				continue;
 			String[] membersList = memberLine.split(DataConstants.DAT_FILE_SEPRTR);
-			// Integer.ParseInt used since loyalty points parameter is in int
-			// type
-			// type
 
 			members.add(new Member(membersList[0], membersList[1], Integer.parseInt(membersList[2])));
 		}
-		// System.out.println(members.toString());
 
 		return members;
 	}
@@ -76,23 +72,15 @@ public class MemberManager {
 	 * @throws IOException
 	 */
 	public String memberSearch(String identifier) throws IOException {
-		// String value="";
 		ArrayList<Member> member = getAllMembers();
 		for (Member memberLine : member) {
-			/*
-			 * System.out.println("our identifier "+identifier+" line1::
-			 * memberLine.getIdentifier()); System.out.println("boolean ::
-			 * memberLine.getIdentifier().equals(identifier));
-			 */
 			if (memberLine.getIdentifier().equals(identifier)) {
-				String value = memberLine.getName() + "," + memberLine.getLoyaltyPoints();
-				return value;
+				return memberLine.getName() + DataConstants.DAT_FILE_SEPRTR + memberLine.getLoyaltyPoints();
 			}
 
 		}
-		// System.out.println(value);
 
-		return "member not found";
+		return DataConstants.MEMBER_NOT_FOUND;
 	}
 
 	/**
@@ -102,9 +90,8 @@ public class MemberManager {
 	 */
 
 	public void addNewMember(String name) throws IOException {
-		int loyaltyPoints = -1;
 		String identifier = UUID.randomUUID().toString().substring(0, 6);
-		memberData.add(new Member(name, identifier, loyaltyPoints));
+		memberData.add(new Member(name, identifier, DataConstants.LOYALTY_NEW_MEMBER));
 	}
 
 	/**
@@ -120,28 +107,11 @@ public class MemberManager {
 			if (memberLine.getIdentifier().equals(identifier)) {
 				memberData.delete(memberLine);
 			} else {
-				System.out.println("member not found");
+				System.out.println(DataConstants.MEMBER_NOT_FOUND);
 			}
 
 		}
-		// System.out.println(value);
 
 	}
-
-	/**
-	 * Checked values using main method for this class alone
-	 * 
-	 * @return member name and loyalty points if id given
-	 * @throws FileNotFoundException,IOException
-	 */
-	/*
-	 * public static void main(String[] args) throws FileNotFoundException,
-	 * IOException { MemberManager m=new MemberManager(); m.addNewMember("dee");
-	 * m.getAllMembers(); String
-	 * value=m.memberSearch("9896c2a4-64f1-4a28-a0fa-312537492775");
-	 * System.out.println(value); m.deleteMember("cf617");
-	 * 
-	 * }
-	 */
 
 }
