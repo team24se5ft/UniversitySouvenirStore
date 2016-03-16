20f9a6e07c07c7b3c8d9aecf393105bc592f6d85
Samrat Shaw <footyapps27@gmail.com>
2016-03-16 12:16:45 +0800

    Added ViewConstants class.

---
+++ UniversitySouvenirStore/src/sg/edu/nus/iss/universitystore/constants/ViewConstants.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package sg.edu.nus.iss.universitystore.constants;
+
+/**
+ * @author Samrat
+ *
+ */
+public class ViewConstants {
+
+	public class Fonts {
+		
+	}
+	
+	public class MenuOptions {
+		
+	}
+	
+	public class Labels {
+		
+	}
+	
+	public class TableHeaders {
+		
+	}
+	
+	public class PaneHeaders {
+		
+	}
+}
