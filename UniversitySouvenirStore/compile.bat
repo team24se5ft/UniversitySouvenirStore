841f7e091c456464489a13d0849e128e8785cc5c
sanjayc <sanjay.choondal@u.nus.edu>
2016-03-20 19:03:08 +0800

    Added bat files to execute code Added Junit Suite
    Signed-off-by: sanjayc <sanjay.choondal@u.nus.edu>

---
+++ UniversitySouvenirStore/compile.bat
@@ -0,0 +1,2 @@
+call setenv.bat
+javac -d classes -sourcepath src test\sg\edu\nus\iss\universitystore\test\InitializeTest.java src\sg\edu\nus\iss\universitystore\MainFrame.java
---
+++ UniversitySouvenirStore/test/sg/edu/nus/iss/universitystore/testsuite/AllTests.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.universitystore.testsuite;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+import sg.edu.nus.iss.universitystore.data.InventoryManagerTest;
+
+@RunWith(Suite.class)
+@SuiteClasses({InventoryManagerTest.class})
+public class AllTests {
+
+}
