7c60b5f51870dca37dd3f88c89d9eb04c805f405
sanjayc87 <sanjay.choondal@live.com>
2016-04-02 17:48:40 +0800

    Added author to MainFrame Removed Unwanted comments
    Signed-off-by: sanjayc87 <sanjay.choondal@live.com>

--- UniversitySouvenirStore/src/sg/edu/nus/iss/universitystore/MainFrame.java
+++ UniversitySouvenirStore/src/sg/edu/nus/iss/universitystore/MainFrame.java
@@ -9,69 +9,69 @@ import sg.edu.nus.iss.universitystore.constants.ViewConstants;
 import sg.edu.nus.iss.universitystore.controller.LoginController;
 import sg.edu.nus.iss.universitystore.view.MainPanel;
 
-public class MainFrame extends JFrame{
-	
+/**
+ * Entry Point for the application
+ * 
+ * @author Samrat
+ *
+ */
+public class MainFrame extends JFrame {
+
 	/***********************************************************/
-	//Constants
-	/***********************************************************/
-	
-	
-	/***********************************************************/
-	//Instance Variables
+	// Instance Variables
 	/***********************************************************/
 	private MainPanel mainPanel;
 	private static final long serialVersionUID = 1L;
 
 	/***********************************************************/
-	//Getters & Setters
+	// Getters & Setters
 	/***********************************************************/
 	public MainPanel getMainPanel() {
 		return mainPanel;
 	}
 
 	/***********************************************************/
-	//Public Methods
+	// Public Methods
 	/***********************************************************/
 	public void createAndShowGUI() {
-		//Initialize the content pane
+		// Initialize the content pane
 		this.mainPanel = new MainPanel();
-		//Setup the Frame
-		this.setSize (ViewConstants.MainFrame.WINDOW_WIDTH, ViewConstants.MainFrame.WINDOW_HEIGHT);
+		// Setup the Frame
+		this.setSize(ViewConstants.MainFrame.WINDOW_WIDTH, ViewConstants.MainFrame.WINDOW_HEIGHT);
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-		//Add components and stuff
+		// Add components and stuff
 		this.setContentPane(mainPanel);
-		//Optional: center on screen
+		// Optional: center on screen
 		this.setLocationRelativeTo(null);
 		this.setVisible(true);
 	}
 
 	/***********************************************************/
-	//Main
+	// Main
 	/***********************************************************/
 	public static void main(String[] args) {
-		//Take the menu bar off the frame
+		// Take the menu bar off the frame
 		System.setProperty(ViewConstants.MainFrame.APPLE_MENUBAR, Boolean.TRUE.toString());
 
-		//Set the look and feel
+		// Set the look and feel
 		try {
-            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
 		} catch (UnsupportedLookAndFeelException ex) {
-            ex.printStackTrace();
-        } catch (IllegalAccessException ex) {
-            ex.printStackTrace();
-        } catch (InstantiationException ex) {
-            ex.printStackTrace();
-        } catch (ClassNotFoundException ex) {
-            ex.printStackTrace();
-        }
-
+			ex.printStackTrace();
+		} catch (IllegalAccessException ex) {
+			ex.printStackTrace();
+		} catch (InstantiationException ex) {
+			ex.printStackTrace();
+		} catch (ClassNotFoundException ex) {
+			ex.printStackTrace();
+		}
 
 		SwingUtilities.invokeLater(new Runnable() {
 			@Override
 			public void run() {
 				MainFrame mainFrame = new MainFrame();
 				mainFrame.createAndShowGUI();
-				//Add the login screen on launch
+				// Add the login screen on launch
 				LoginController loginController = new LoginController();
 				mainFrame.getMainPanel().add(loginController.getLoginPanel());
 			}
